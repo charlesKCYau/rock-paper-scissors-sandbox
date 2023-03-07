4e432d66ea416cd654e10387cd019aaafc4efdbf
yaunga@hotmail.com <yaunga@hotmail.com>
2023-03-07 17:40:40 +0000

    finished

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ const getRandomMove = () => {
   const resetGame = () => {
     if (document.getElementById("outcome")) {
       const outcome = document.body.lastChild;
-      document.body.removeChild(outcome);
+      // document.body.removeChild(outcome);
+      document.getElementById("outcome").innerText = "";
     }
   };
   
@@ -39,6 +40,7 @@ const getRandomMove = () => {
   const updateDOM = (moveOne, moveTwo, outcome) => {
     // TODO Implement this method to update the DOM
     // There are some images you can use in the images directory
+    console.log(moveOne, moveTwo, outcome);
     const playerOneImgId = document.body.querySelector("#player-one-move__img");
     const playerTwoImgId = document.body.querySelector("#player-two-move__img");
     if (moveOne === "rock") {
@@ -50,14 +52,14 @@ const getRandomMove = () => {
     }
     if (moveTwo === "rock") {
       playerTwoImgId.src="./images/rock.png";
-    } else if (moveOne === "paper") {
+    } else if (moveTwo === "paper") {
       playerTwoImgId.src="./images/paper.png";
-    } else if (moveOne === "scissors") {
+    } else if (moveTwo === "scissors") {
       playerTwoImgId.src="./images/scissors.png";
     }
     if (document.getElementById("outcome")) {
       document.getElementById("outcome").innerText = outcome;
-    }
+    } 
 
   };
   
